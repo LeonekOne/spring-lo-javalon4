615521f9ef98618bd96e23f0a3b2f2568269ed67
Leszek <leonekjr@poczta.onet.pl>
2021-02-21 11:42:52 +0100

    [change] - playing with beans examples :)

---
+++ src/main/java/pl/sda/javalondek4springdemo/repository/BookRepository.java
@@ -0,0 +1,32 @@
+package pl.sda.javalondek4springdemo.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.sda.javalondek4springdemo.model.Book;
+import pl.sda.javalondek4springdemo.service.MyService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(BookRepository.class);
+
+    private List<Book> books;
+
+    public BookRepository() {
+        List<Book> someBooks = List.of(
+            new Book(1L, "Henryk Sienkiewicz", "Potop"),
+            new Book(2L, "Adam Mickiewicz", "Pan Tadeusz"),
+            new Book(3L, "Prus", "Lalka")
+        );
+
+        this.books = new ArrayList<>(someBooks);
+
+        logger.info("book repository initialized with books: {}", books);
+    }
+
+
+
+
+}
