96dc5ad08ae9032c0cee970493a0a7d52d4601d3
Leonek <leonekjr@poczta.onet.pl>
2021-03-20 09:38:10 +0000

    [add] - example of bean creations

---
+++ src/main/java/pl/sda/javalondek4springdemo/bean/Team.java
@@ -0,0 +1,18 @@
+package pl.sda.javalondek4springdemo.bean;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Team {
+
+    public final Player whichPlayer;
+
+    public Team(@Qualifier("buffon") Player whichPlayer) {
+        System.out.println("Injected class: " + whichPlayer.getClass());
+        this.whichPlayer = whichPlayer;
+    }
+
+
+
+}
