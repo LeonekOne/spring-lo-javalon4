4590965cb9c6bbc9abba15447159c0ee1cf5f4ae
Leonek <leonekjr@poczta.onet.pl>
2021-03-20 11:47:33 +0000

    [add] - @OneToMany and @ManyToOne relations implemented

--- src/main/java/pl/sda/javalondek4springdemo/model/Course.java
+++ src/main/java/pl/sda/javalondek4springdemo/model/Course.java
@@ -1,44 +1,44 @@
 package pl.sda.javalondek4springdemo.model;
-
 import javax.persistence.*;
 
 @Entity
 public class Course {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String courseName;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "TEACHER_FOREIGN_ID", referencedColumnName = "ID")
     private Teacher teacher;
-
     public Course() {
         // empty
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getCourseName() {
         return courseName;
     }
-
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
-
     public Teacher getTeacher() {
         return teacher;
     }
-
     public void setTeacher(Teacher teacher) {
         this.teacher = teacher;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", courseName='" + courseName + '\'' +
+                ", teacher=..." +
+                '}';
     }
 }
--- src/main/java/pl/sda/javalondek4springdemo/model/Teacher.java
+++ src/main/java/pl/sda/javalondek4springdemo/model/Teacher.java
@@ -1,46 +1,49 @@
 package pl.sda.javalondek4springdemo.model;
-
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.List;
-import java.util.UUID;
-
 @Entity
 public class Teacher {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private UUID id;
-
+//    @Basic
+    private Long id;
     @Embedded
     private Address address;
-
-    @OneToMany
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "teacher")
     private List<Course> courseList;
-
     public Teacher() {
     }
-
-    public UUID getId() {
+    public Long getId() {
         return id;
     }
-
-    public void setId(UUID id) {
+    public void setId(Long id) {
         this.id = id;
     }
-
     public Address getAddress() {
         return address;
     }
-
     public void setAddress(Address address) {
         this.address = address;
     }
-
     public List<Course> getCourseList() {
         return courseList;
     }
-
     public void setCourseList(List<Course> courseList) {
         this.courseList = courseList;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", address=" + address +
+                ", courseList=" + courseList +
+                '}';
     }
 }
