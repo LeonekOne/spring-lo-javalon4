2ffa9a68bfdd6dae417b0faba137aac2a548929e
Leonek <leonekjr@poczta.onet.pl>
2021-03-21 10:24:05 +0000

    [change] - @OneToOne bidirectional in progress...

--- src/main/java/pl/sda/javalondek4springdemo/model/onetoone/bidirectional/OneToOneBidirectionalExample.java
+++ src/main/java/pl/sda/javalondek4springdemo/model/onetoone/bidirectional/OneToOneBidirectionalExample.java
@@ -1,5 +1,4 @@
 package pl.sda.javalondek4springdemo.model.onetoone.bidirectional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -7,36 +6,28 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import pl.sda.javalondek4springdemo.repository.onetoone.bidirectional.CatRepository;
 import pl.sda.javalondek4springdemo.repository.onetoone.bidirectional.OwnerRepository;
-
-
-@Profile("onetoone-bidirectional")
+@Profile("onetoone-birectional")
 @Component
 public class OneToOneBidirectionalExample implements CommandLineRunner {
-
     private static final Logger logger = LoggerFactory.getLogger(OneToOneBidirectionalExample.class);
-
     private final OwnerRepository ownerRepository;
-
     private final CatRepository catRepository;
-
     public OneToOneBidirectionalExample(OwnerRepository ownerRepository, CatRepository catRepository) {
         this.ownerRepository = ownerRepository;
         this.catRepository = catRepository;
     }
-
     @Override
     public void run(String... args) throws Exception {
         Owner me = new Owner();
-        me.setFirstName("Leonko");
-        me.setLastName("O.");
-
+        me.setFirstName("maniek");
+        me.setLastName("p.");
         Cat mruczek = new Cat();
-        mruczek.setName("Mruczek");
-
-//        me.setCat(mruczek);
-//        mruczek.setOwner(me);
-
-        // save to DB
+        mruczek.setName("mruczek");
+        catRepository.save(mruczek);
+        logger.info("mruczek was saved to db with id: [{}]", mruczek.getId());
+//
+        me.setCat(mruczek);
         ownerRepository.save(me);
+        mruczek.setOwner(me);
     }
 }
--- src/main/java/pl/sda/javalondek4springdemo/repository/onetoone/bidirectional/CatRepository.java
+++ src/main/java/pl/sda/javalondek4springdemo/repository/onetoone/bidirectional/CatRepository.java
@@ -2,8 +2,8 @@ package pl.sda.javalondek4springdemo.repository.onetoone.bidirectional;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
-import pl.sda.javalondek4springdemo.model.onetoone.bidirectional.Owner;
+import pl.sda.javalondek4springdemo.model.onetoone.bidirectional.Cat;
 
 @Profile("onetoone-bidirectional")
-public interface CatRepository extends CrudRepository<Owner, Long> {
+public interface CatRepository extends CrudRepository<Cat, Long> {
 }
