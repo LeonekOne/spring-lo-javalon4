065f1db98a2eeeb5b3c6f8456e4ed1933b72d5df
Leonek <leonekjr@poczta.onet.pl>
2021-03-28 10:31:08 +0100

    [change] - get communication with RestTemplate done (working)

--- src/main/java/pl/sda/javalondek4springdemo/service/github/GithubRestService.java
+++ src/main/java/pl/sda/javalondek4springdemo/service/github/GithubRestService.java
@@ -6,22 +6,33 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 
 @Service
 public class GithubRestService {
 
     private static final Logger logger = LoggerFactory.getLogger(GithubRestService.class);
 
+    private static final String allRepositoriesUrl = "https://api.github.com/users/{user}/repos";
+
     private final RestTemplate restTemplate;
 
     private final String gitHubUser;
 
-    public GithubRestService(RestTemplate restTemplate, @Value("${github.user}") String gitHubUser,
+    public GithubRestService(RestTemplate restTemplate,
+                             @Value("${github.user}") String gitHubUser,
                              @Value("${magic.value}") int magicValue) {
         this.restTemplate = restTemplate;
         this.gitHubUser = gitHubUser;
 
-        logger.info("github user: [{}]", gitHubUser);
-        logger.info("magic value: [{}]", magicValue);
+        logger.info("github user: [{}], magic value: [{}]", gitHubUser, magicValue);
+
+    }
+
+    public String findMyRepoAsString() {
+        return restTemplate.getForObject(allRepositoriesUrl, String.class, gitHubUser);
+//        Map<String, String> parametersNamesOverValues = Map.of("user", gitHubUser);
+//        return restTemplate.getForObject(allRepositoriesUrl, String.class, parametersNamesOverValues);
     }
 }
